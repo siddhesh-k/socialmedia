cc378699e80ba1fb9efcb0facb7499a9931de009
siddhesh khadapkar <siddhkhadapkar22@gmail.com>
2022-04-26 19:04:20 +0530

    added .gitignore

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
+
+class Posts(models.Model):
+    title = models.CharField(max_length=20)
+    description = models.CharField(max_length=100)
+    poster = models.ImageField(upload_to="posts")
+    tags = models.ManyToManyField(Tag)
+    likes = models.IntegerField()
+    comments = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now="true")
+    def __str__(self):
+        return self.title
+
+
+
+class User(models.Model):
+    username= models.CharField(max_length=50,unique="true")
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    profile_picture = models.ImageField(upload_to="profiles")
+    posts = models.ForeignKey(Posts,on_delete=models.CASCADE)
+    email = models.EmailField()
+    mobile = models.CharField(max_length=10)
+    is_active = models.BooleanField(default="true")
+    created_at = models.DateTimeField(auto_now="true")
+    def __str__(self):
+        return self.username
