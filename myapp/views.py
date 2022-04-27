659745e32a0eb16b484993d80df3c9fc575682f1
siddhesh khadapkar <siddhkhadapkar22@gmail.com>
2022-04-28 04:31:56 +0530

    created add post and add user

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import User
+from .models import User,Posts,Tag
 
 # Create your views here.
 def home(request):
@@ -9,17 +9,49 @@ def signIn(request):
     return render(request,'sign_in.html')
 
 
-def create(request):
+def create_user(request):
     if request.method == "POST":
-        username = request.POST['name']
+        username = request.POST['username']
         firstname = request.POST['first_name']
         last_name = request.POST['last_name']
         password= request.POST['password']
-        profile_picture = request.POST['profile_picture']
         email= request.POST['email']
-        mobile= request.POST['mobile']
-        obj = User.objects.create(username=username,first_name= firstname,last_name=last_name,
-                                  password=password,profile_picture=profile_picture,
-                                  email=email,mobile=mobile)
+        obj = User.objects.create(username=username,first_name= firstname,last_name=last_name,password=password,email=email)
         obj.save()
-        return redirect('/signup')
+        return redirect('/')
+
+def add_post(request):
+
+    if request.method == "POST":
+        print(request.FILES)
+        title = request.POST['title']
+        poster = request.FILES['poster']
+        description = request.POST['description']
+        try:
+            user= User.objects.get(pk=1)
+            obj = Posts.objects.create(title=title, description=description,poster=poster,created_by=user)
+            # obj.tags.set([tags])
+            obj.save()
+        except Exception as e:
+            print(e)
+            return redirect('/')
+        def extract_hashtags(text):
+
+            # initializing hashtag_list variable
+            hashtag_list = []
+
+            # splitting the text into words
+            for word in text.split():
+
+                # checking the first character of every word
+                if word[0] == '#':
+                    # adding the word to the hashtag_list
+                    hashtag_list.append(word[1:])
+
+            # printing the hashtag_list
+            print("The hashtags in \"" + text + "\" are :")
+            for hashtag in hashtag_list:
+                print(hashtag)
+
+        return redirect('/')
+    return render(request,'add_post.html')
